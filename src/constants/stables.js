db39cd437da60b84b07cd324866cdd14d3bb8b4b
VijayaSP <144252779+VijayaSP@users.noreply.github.com>
2025-05-20 21:47:47 +0530

    Update stables.js

--- src/constants/stables.js
+++ src/constants/stables.js
@@ -1,5 +1,5 @@
-const UPLOAD_FOLDER_BASE_URL = "http://localhost:5000/uploads/";
-const API_BASE_URL = "http://localhost:5000";
+const UPLOAD_FOLDER_BASE_URL = `${process.env.REACT_APP_API_BASE_URL}/uploads/`;
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 const stables = { UPLOAD_FOLDER_BASE_URL, API_BASE_URL };
 
